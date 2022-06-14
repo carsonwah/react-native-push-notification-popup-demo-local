89d644d4d155555dab89ef1bad8db6388bed136e
Carson Wah <carsonwah@gmail.com>
2022-06-14 19:53:27 +0800

    Import rn-pushnoti for local testing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import NotificationPopup from 'react-native-push-notification-popup';
 
 export default function App() {
+  const childRef = useRef<NotificationPopup>(null);
+
+  const showMessage = () => {
+    childRef.current?.show({
+      onPress: function() {console.log('Pressed')},
+      appIconSource: require('./assets/icon.png'),
+      appTitle: 'Some App',
+      timeText: 'Now',
+      title: 'Hello World',
+      body: 'This is a sample message.\nTesting emoji 😀',
+      slideOutTime: 5000
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Show Message' onPress={showMessage} />
+      <NotificationPopup ref={childRef} />
       <StatusBar style="auto" />
     </View>
   );
